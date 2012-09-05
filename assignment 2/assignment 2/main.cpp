44a26a9575b66109f63993791e039a154d18a9e0
Jared Everson <wolfeversongithub@gmail.com>
2012-09-05 14:06:45 -0500

    assignment 2 finish

--- assignment 2/assignment 2/main.cpp
+++ assignment 2/assignment 2/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>;
 #include <conio.h>;
 using namespace std;
-
+//Jared Everson
+//assignment 2
+//"cout" arranging text
 int main()
 {
-
-
-
-	return 0
+	//output lines to match the assignment.
+	cout << "Computers, computers everywhere";
+	cout << "\n\tas far as I can C.";
+	cout << "\nI really, really like these things";
+	cout << "\n\tOh joy. Oh joy for me!";
+	getch();
+	return 0;
 }
