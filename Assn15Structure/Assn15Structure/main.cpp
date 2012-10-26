71f75234d91216f1b470973fe7dde96c61971051
Jared Everson <wolfeversongithub@gmail.com>
2012-10-26 13:04:13 -0500

    Assignment 15 EC

--- Assn15Structure/Assn15Structure/main.cpp
+++ Assn15Structure/Assn15Structure/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <conio.h>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct employee
@@ -14,45 +15,48 @@ struct employee
 
 void main()
 {
-		//testdata
-	employee jobs;
-	jobs.num = 3462;
-	jobs.name = "Jobs";
-	jobs.rate = 4.62;
-	jobs.hours = 40;
-	employee hoop;
-	hoop.num = 6793;
-	hoop.name = "Hooper";
-	hoop.rate = 5.83;
-	hoop.hours = 38;
-	//testdata end
+	////testdata
+	//employee jobs;
+	//jobs.num = 3462;
+	//jobs.name = "Jobs";
+	//jobs.rate = 4.62;
+	//jobs.hours = 40;
+	//employee hoop;
+	//hoop.num = 6793;
+	//hoop.name = "Hooper";
+	//hoop.rate = 5.83;
+	//hoop.hours = 38;
+	////testdata end
 	double totalgrass = 0; //assignment asks for "total grass pay for all employees"...rate * hours 
 	bool isQuit = false;
-	employee *empList  =  new employee[2];  //debug
-	empList[0] = jobs;
-	empList[1] = hoop;
-	int empIndex = 2; //2 for debug
+	vector<employee> empList(1);
+	//empList[0] = jobs;
+	//empList[1] = hoop;
+	int empIndex = 0; //2 for testdata
 
 
 	
 	while (!isQuit)
 	{
-		if (empIndex >= (sizeof(empList[0])/sizeof(empList)))
+		//increases vector size to allocate memory as needed
+		if (empIndex >= empList.size())
 		{
-
+			empList.resize(empIndex +5);
 		}
+		//input
 		employee tempEmp;
 		cout << "Employee Name?(last)";
-		getline(cin, tempEmp.name);
+		cin >> tempEmp.name;
 		cout << "Employee Number?";
 		cin >> tempEmp.num;
 		cout << "Employee Hours?";
 		cin >> tempEmp.hours;
 		cout << "Employee Rate?";
 		cin >> tempEmp.rate;
+		//checks loop 
 		if (tempEmp.name != "q")
 		{
-			empList[empIndex] = tempEmp;
+			empList.at(empIndex) = tempEmp;
 			empIndex++;
 		}
 		else
@@ -65,8 +69,8 @@ void main()
 	cout << setw(10) << "Number" << setw(10) << "Name" << setw(10) << "Rate" << setw(10) << "Hours" << endl;
 	for (int i = 0; i < empIndex; i++)
 	{
-		cout << setw(10) << empList[i].num << setw(10) << empList[i].name << setw(10) << empList[i].rate << setw(10) << empList[i].hours;
-		totalgrass += (empList[i].hours * empList[i].rate);
+		cout << setw(10) << empList.at(i).num << setw(10) << empList.at(i).name << setw(10) << empList.at(i).rate << setw(10) << empList.at(i).hours;
+		totalgrass += (empList.at(i).hours * empList.at(i).rate);
 		cout << endl;
 	}
 	cout << endl << endl << "Total for all employees is: " << totalgrass;
