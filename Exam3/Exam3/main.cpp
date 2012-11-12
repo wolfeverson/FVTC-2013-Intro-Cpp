8b6f3f3d3db73bfee2504a06881bf80a0fe5dba9
Jared Everson <wolfeversongithub@gmail.com>
2012-11-12 13:23:09 -0600

    Exam3

---
+++ Exam3/Exam3/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <conio.h>
+//#include <ios>
+//#include <io.h>
+using namespace std;
+
+//Write a C++ program that will ask for N numbers to be put into a 1 dimensional integer array, where N is an integer value also input by the user.  
+//
+//Your program will 
+//	store these numbers in an array 
+//	calculate and display the total, 
+//	and display the numbers in reverse order.
+//
+//Be sure to include appropriate comments and output to the end user to explain the functionality of the program.
+//
+//Extra Credit (up to 10 points) Write these numbers (using the same formatting) out to a text file. 
+
+void main()
+{
+	int input, total = 0;
+	cout << "How many numbers would you like your array to be?";
+	cin >> input;
+	int *numbers = new int[input];
+	for (int i = 0; i < input; i++)
+	{
+		cout << "Please input Integer #" << i << endl;
+		cin >> numbers[i];
+		total += numbers[i];
+	}
+	cout << endl << "The numbers in reverse order are: " << endl;
+	for (int i = input-1; i >= 0; i--)
+	{
+		cout << numbers[i] << endl;
+	}
+	cout << endl << "Your total is: " << total;
+
+	//file1 = new ;    
+	//file1.open("examfile.txt");
+	//file1.write << input;
+	//file1.write << endl << "The numbers in reverse order are: " << endl;
+	//for (int i = input-1; i >= 0; i--)
+	//{
+	//	file1.write << numbers[i] << endl;
+	//}
+	//file1.write << endl << "Your total is: " << total;
+	//file1.close();
+	getch();
+}
